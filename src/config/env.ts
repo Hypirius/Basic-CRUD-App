7a59af5cc53bd93bf1751620d517bfc6225ec862
Hypirius <zuhayerhossain25@gmail.com>
2026-03-22 21:26:47 +0600

    chore: Add support for node_env access

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,21 +3,20 @@ import { fileURLToPath } from "url"
 import { dirname } from "path"
 import { config } from "dotenv"
 
+const NODE_ENV = process.env.NODE_ENV || "development"
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 config({
-  path: path.join(
-    __dirname,
-    "../../",
-    `.env.${process.env.NODE_ENV || "development"}.local`,
-  ),
+  path: path.join(__dirname, "../../", `.env.${NODE_ENV}.local`),
 })
 
 const _config = {
   PORT: process.env.PORT || 3000,
   DB_NAME: process.env.DB_NAME,
   DB_URL: process.env.DB_URL,
+  NODE_ENV,
   __dirname,
   __filename,
 }
